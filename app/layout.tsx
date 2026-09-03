82754758c8b28b354437581eca40b20cc6a737cb
SELVA7092 <selvarockz02@email.com>
2026-09-03 13:03:00 +0530

    Save all current changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 
 import "./globals.css";
 import Navigation from "./components/Navigation";
@@ -38,6 +37,12 @@ export default function RootLayout({
           name="google-adsense-account"
           content="ca-pub-4333070677760037"
         />
+
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4333070677760037"
+          crossOrigin="anonymous"
+        />
       </head>
 
       <body className="min-h-full flex flex-col">
@@ -48,14 +53,6 @@ export default function RootLayout({
         </main>
 
         <Footer />
-
-        {/* Google AdSense */}
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4333070677760037"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
